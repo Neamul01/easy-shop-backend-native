47481b2d9fa31b8de4b47132efebda347b023217
Neamul01 <neamulhaque2002@gmail.com>
2023-10-29 02:51:19 +0600

    user api added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
 
 app.use(cors());
 app.options("*", cors());
@@ -11,6 +13,8 @@ app.options("*", cors());
 // middleware
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(authJwt());
+app.use(errorHandler);
 
 // Routers
 const productsRoutes = require("./routes/products");
@@ -20,9 +24,9 @@ const ordersRoutes = require("./routes/orders");
 
 const api = process.env.API_URL;
 
+app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/categories`, categoriesRoutes);
-app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
 mongoose
